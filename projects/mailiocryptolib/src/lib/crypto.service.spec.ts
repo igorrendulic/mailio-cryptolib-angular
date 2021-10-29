c9ba1f0592f04d2c00cdcb4c37e08ce4f5503bd3
Igor Rendulic <igor@mail.io>
2021-10-29 17:32:33 -0600

    Added sjcl with sha512 to the project

--- projects/mailiocryptolib/src/lib/crypto.service.spec.ts
+++ projects/mailiocryptolib/src/lib/crypto.service.spec.ts
@@ -38,4 +38,12 @@ describe('CryptoService', () => {
     expect(pairs.signKeyPair.secretKey.length).toEqual(64);
   });
 
+  it('calculate sha256', () => {
+     expect(service.sha256('test')).toEqual('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
+  });
+
+  it('calculate sha512', () => {
+    expect(service.sha512('test')).toEqual('ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff');
+  });
+
 });
